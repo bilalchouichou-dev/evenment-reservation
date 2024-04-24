db9231451365b783356a32da6c7a531ec483ddbc
BilalChouichou <148156086+BilalChouichou@users.noreply.github.com>
2024-04-24 21:18:53 +0100

    second commit

--- src/components/EventDetailMain.js
+++ src/components/EventDetailMain.js
@@ -33,7 +33,7 @@ function EventsMain({ titre, ville, imageSrc, date, prix, location, description,
         <section className=" p-16">
                 <h2 className="text-2xl font-semibold mb-4">Localisation</h2>
                 <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26390.40115194911!2d-4.0454636652343865!3d34.22812109999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd9e1706ecb23995%3A0x6cd1576de83d6d65!2sTaza!5e0!3m2!1sfr!2sma!4v1713956536659!5m2!1sfr!2sma"
+                    src={location}
                     className="h-[30rem] w-6/12"
                     style={{ border: '0' }}
                     loading="lazy"
